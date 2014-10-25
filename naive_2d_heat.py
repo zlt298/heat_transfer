4f1d82c69845e725ed9c3159aab45e53db9806b8
zlt298 <zealot298@hotmail.com>
2014-10-24 16:27:03 -1000

    v0.1
    Reworked initialization of Grid and Node
    Partially implemented boundary conditions

--- naive_2d_heat.py
+++ naive_2d_heat.py
@@ -1,3 +1,4 @@
+import numpy as np
 """
 A simple implementation of a rectangular grid 2D steady state heat transfer 
 Grid is sized L x H, and the mesh size is m x n
@@ -16,7 +17,7 @@ Boundary conditions include:
 """
 
 
-class grid():
+class Grid(object):
     def __init__(self,dim_size,mesh_size,k,q_dot):
         """create a grid with the following parameters:
         dim_size: Size of the area as a tuple (L,H)
@@ -24,98 +25,150 @@ class grid():
         k: Coefficient of conduction
         q_dot: Heat generation
         """
+        self.args = (dim_size,mesh_size,k,q_dot)
         self.DIMX,self.DIMY = dim_size
         self.M,self.N = mesh_size
         self.DX,self.DY = self.DIMX/float(self.M),self.DIMY/float(self.N)
         self.K = k
         self.QDOT = q_dot
+        
+        self.mat_id = [(x,y) for y in range(self.N) for x in range(self.M)]
+        self.flat_id = range(len(self.mat_id))
+        self.matToflat = dict(zip(self.mat_id,self.flat_id))
+        self.matToflat[(-1,-1)] = -1
 
     def init_mesh(self):
-        self.mesh = [[cell(x,y,(0,(0,None),0)) for x in range(self.M)] for y in range(self.N)]
+        self.mesh = [Node(x,y,(0,(0,None),0),*self.args) for x,y in self.mat_id]
         self.temperature = np.zeros((self.M,self.N))
         for y in range(self.M):
             for x in range(self.N):
-                self.Temperature[x,y] = self.mesh[x][y].current_temp
+                pass
         
     def __str__(self):
         s = ''
         for x in range(self.M):
-            s = s + ' | '.join([str(ss) for ss in self.Temperature[x,:]]) + '\n'
+            s = s + ' | '.join(['%3.f'%ss for ss in self.temperature[x,:]]) + '\n'
         return s
 
-    def evaluate(self,targeterror = 0.1):
-        return None
+    def validate_bounds(self):
+        return False
+    
+    def evaluate(self):
+        if self.validate_bounds():
+            return None
+        else:
+            print "Wall's are not properly bounded"
+            return None
 
-    def show_temp(self,):
+    def show_temp(self):
         return None
     
-class cell(grid):
-    def __init__(self,ypos,xpos,bc):
-        """create a cell with the following parameters:
-        ypos: Position in the Y, 0 indexed
+class Node(Grid):
+    def __init__(self,ypos,xpos,bc,*args):
+        """create a node with the following parameters:
         xpos: Position in the X, 0 indexed
-        dy: cell wall size
-        dx: cell wall size
+        ypos: Position in the Y, 0 indexed
+        dy: node wall size
+        dx: node wall size
         bc:
             The boundary condition for the specified side, given as a tuple:
-            (type,value):
-                type:
-                    0 = no boundary condition,value as 0 or None
-                    1 = constant temperature condition, value as float of the temperature in K
-                    2 = constant flux condition, value as float of the flux in watts
-                    3 = convection condition, value as tuple, (h,tinf)
-            eg. bcTop = (2,-5000),bcRight = (3,(0.6,310)),bcBot = (0,), bcLeft = (0,)
+            (Configuration, Condition, Rotation):
+                Configuration
+                    internal node = 0, wall node = 1, Internal Corner = 2, External Corner = 3
+                Condition
+                    None = (0,None)
+                    Constant Temperature = (1, T)
+                    Constant Flux = (2,q")
+                    Convection = (3,h,Tinf)
+                Rotation
+                    right,up,left,down = 0,1,2,3
         """
+        self.DIMX,self.DIMY = args[0]
+        self.M,self.N = args[1]
+        self.DX,self.DY = self.DIMX/float(self.M),self.DIMY/float(self.N)
+        self.K = args[2]
+        self.QDOT = args[3]
+        
         self.Y,self.X = ypos,xpos
-        self.house = [super.mesh[self.Y-1][self.X],super.mesh[self.Y][self.X-1],
-                  super.mesh[self.Y+1][self.X],super.mesh[self.Y][self.X+1]]
-        
         self.current_temp = 0.
-        self.prev_temp = 0.
-        self.error = 1e10
-        self.bound = (bcTop,bcLeft,bcBot,bcRight)
-        self.bounds = sum(val[0]>0 for val in self.bound)
-        self.updateBC()
+        self.bound = bc
         
-    def updateBC():
-        self.CONSTANTTEMP = sum([t[1] for t in self.bound if t[0]==1])
-        if self.CONSTANTTEMP:self.CONSTANTTEMP = self.CONSTANTTEMP/float(len([None for t in self.bound if t[0]==1]))
+        self.update_bc()
+        
+    def update_bc(self):
+        pos = (self.X,self.Y)
+        house = [(self.X+1,self.Y),(self.X,self.Y+1),(self.X-1,self.Y),(self.X,self.Y-1)]
 
-        dxsq,dysq = pow(super.DX,2),pow(super.DY,2)
-        self.equation = lambda z:(dysq*(self.house[1].current_temp+self.house[3].current_temp)+
-                                  dxsq*(self.house[0].current_temp+self.house[2].current_temp)+
-                                  super.QDOT/float(super.K)*dysq*dxsq)/float(2*(dxsq+dysq))
-        #Handle Boundary Conditions
-        if self.CONSTANTTEMP: #Temperature Constraint overrides all else
-            self.current_temp = self.CONSTANTTEMP
-        elif self.bounds == 1:
-            dxy = [(super.DY,super.DX),(super.DX,super.DY),(super.DY,super.DX),(super.DX,super.DY)]
-            pos = [val[0]>0 for x in a].index(1)
-            if sum(val[0] for val in self.bound) == 2:   # Plane Wall q
-                self.equation = lambda z:(super.DY*(self.neighbor[1]+self.neighbor[3])+super.DX*(self.neighbor[0]+self.neighbor[2])+
-                                          super.QDOT/float(super.K)*pow(super*DY,2)*pow(super.DX,2))/float(2*(super.DX+super.DY))
-            else:                                        # Plane Wall h
-                pass
-        elif self.bounds == 2:
-            if sum(val[0] for val in self.bound) == 4:   # Corner q-q
-                pass
-            elif sum(val[0] for val in self.bound) == 6: # Corner h-h
-                pass
-            else:                                        # Corner h-q
-                pass
-        elif self.bounds == 0:
-            pass
-        else:
-            print 'Cell is overconstrained. Check (%s,%s)'%(self.Y,self.X)
+        dx,dy,dxsq,dysq = self.DX,self.DY,pow(self.DX,2),pow(self.DY,2)
+        rotd = [dysq,dxsq,dysq,dxsq]
+        rotdsq = [dy,dx,dy,dx]
 
-    def setBC(side,val):
-        tempbound = list(self.bound)
-        tempbound[side] = val
-        self.bound = tuple(tempbound)
-        self.bounds = sum(val[0]>0 for val in self.bound)
-        self.updateBC()
+        conf = self.bound[0]
+        rot = self.bound[2]
+        genConst = -self.QDOT*dxsq*dysq/float(self.K)
+        posConst = -2*(dxsq+dysq)
+        
+        #Handle Boundary Conditions, see writeup for derivations
+        #Null Condition
+        if self.bound[1][0] == 0:
+            self.coeff =     [(house[0],            dysq),
+                              (house[1],            dxsq),
+                              (house[2],            dysq),
+                              (house[3],            dxsq),
+                              (pos,                 posConst),
+                              ((-1,-1),             genConst)]
+
+        #Constant Temperature Condition
+        elif self.bound[1][0] == 1:
+            self.coeff =     [(self.pos,1),
+                              ((-1,-1),             self.bound[1][1])]
+
+        #Constant Flux Condition
+        elif self.bound[1][0] == 2:
+            q = self.bound[1][1]
+            #Wall Configuration
+            if conf == 1:
+                self.coeff = [(house[rot],          0),
+                              (house[(rot+1)%4],    rotdsq[(rot+1)%4]),
+                              (house[(rot+2)%4],    2*rotdsq[(rot+2)%4]),
+                              (house[(rot+3)%4],    rotdsq[(rot+3)%4]),
+                              (pos,                 posConst),
+                              ((-1,-1),             genConst-(2*q*rotdsq[rot]/float(self.K)))]
+            
+            #Internal Corner Configuration
+            elif conf == 2:
+                self.coeff = [(house[rot],          rotdsq[rot]),
+                              (house[(rot+1)%4],    rotdsq[(rot+1)%4]),
+                              (house[(rot+2)%4],    2*rotdsq[(rot+2)%4]),
+                              (house[(rot+3)%4],    rotdsq[(rot+3)%4]),
+                              (pos,                 posConst),
+                              ((-1,-1),             genConst-(2*q*rotdsq[rot]/float(self.K)))]
+            
+            #External Corner Configuration
+            elif conf == 3:
+                pass
+
+        #Constant Convection Condition
+        elif self.bound[1][0] == 3:
+            h,Tinf = self.bound[1][1],self.bound[1][2]
+            #Wall Configuration
+            if conf == 1:
+                pass
+            
+            #Internal Corner Configuration
+            elif conf == 2:
+                pass
+            
+            #External Corner Configuration
+            elif conf == 3:
+                pass
+
+    def set_bc(self,bound):
+        self.bound = bound
+        self.update_bc()
         
 if __name__ == '__main__':
-    g = grid((100,100),(1000,1000),20,0)
+    g = Grid((100,100),(25,25),20,1)
+    g.init_mesh()
     for m in range(1000):
         pass
